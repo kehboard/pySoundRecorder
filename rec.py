5e0fbd519e779d74f0c09cdf4fc59eeff3db66bc
kehboard <kehboard@github.com>
2021-02-24 14:46:25 +0200

    Починил отображение времени и попытался уменьшить задержку.

--- rec.py
+++ rec.py
@@ -8,6 +8,7 @@ import wavio as wv
 import os
 import signal
 
+
 class RecordThread(threading.Thread):
     # конструктор класса
     def __init__(self, use_mic, use_speaker, filename, stop_event):
@@ -15,40 +16,61 @@ class RecordThread(threading.Thread):
         self.use_mic = use_mic
         self.use_speaker = use_speaker
         self.stop_event = stop_event
+        self.filename = filename
+
     def run(self):
-        recorded_data = np.empty((0, 2), np.float32)  # создаем массив в котором будет хранится запись с микрофона
+        recorded_data = []
+        # recorded_data = np.empty((0, 2), np.float32)  # создаем массив в котором будет хранится запись с микрофона
         with self.use_mic.recorder(samplerate=48000) as mic, \
-                self.use_speaker.player(samplerate=48000) as sp: # получаем объекты Recorder и Player для записи и проигывания аудио
+                self.use_speaker.player(
+                    samplerate=48000) as sp:  # получаем объекты Recorder и Player для записи и проигывания аудио
             start_record_time = time.time()  # получаем время когда начали запись
             while time.time() - start_record_time < 60:
                 data = mic.record()  # получаем те данные которые есть сейчас без буфферизации
                 sp.play(data)  # проигрываем записанные данные в динамик
-                recorded_data = np.append(recorded_data, data)  # дописываем данные в массив
-                if stop_event.isSet() and time.time() - start_record_time >= 5:  # если пользователь прервал запись
-                    recorded_data *= 2  # усиливаем записанное аудио в 2 раза
-                    # т.к библиотека soundcard пишет в float32 а некоторые плееры не умеют воспроизводить .wav
-                    # записанный в таком формате нам надо преобразовать float32 в int16
-                    recorded_data = self.float2pcm(recorded_data)
-                    # сохраняем в файл
-                    wv.write(filename, recorded_data, 96000)
-                    return  # и время записи больше чем 5 секунд то прерываем цикл
-        recorded_data *= 2  # усиливаем записанное аудио в 2 раза
-        # т.к библиотека soundcard пишет в float32 а некоторые плееры не умеют воспроизводить .wav
-        # записанный в таком формате нам надо преобразовать float32 в int16
-        recorded_data = self.float2pcm(recorded_data,"int16")
-        # сохраняем в файл
-        wv.write(filename, recorded_data, 96000)
+                # recorded_data = np.append(recorded_data, data)  # дописываем данные в массив
+                recorded_data += np.ndarray.tolist(data)
+                self.dur = time.time() - start_record_time
+                if stop_event.isSet() and self.dur >= 5:  # если пользователь прервал запись
+                    self.save(filename, recorded_data)
+                    return # и время записи больше чем 5 секунд то прерываем цикл
+        self.save(filename, recorded_data)
         # шлем SIGINT чтобы сбросить input()
         os.kill(os.getpid(), signal.SIGINT)
 
-    # Функция для преобразования float32 в pcm массива numpy.
+    def save(self, fname, data):
+        data = self.convert2numpy(data)  # конвертируем в массив numpy т.к. wavio принимает на вход
+        data *= 2  # усиливаем записанное аудио в 2 раза
+        # т.к библиотека soundcard пишет в float32 а некоторые плееры не умеют воспроизводить .wav
+        # записанный в таком формате нам надо преобразовать float32 в int16
+        data = self.float2pcm(data)
+        # сохраняем в файл
+        wv.write(fname, data, 48000)
+
+    # Функция для преобразования float32 в pcm массив numpy.
     # Взята с https://github.com/mgeier/python-audio/blob/master/audio-files/utility.py
     def float2pcm(self, sig, dtype='int16'):
         sig = np.asarray(sig)
+        if sig.dtype.kind != 'f':
+            raise TypeError("'sig' must be a float array")
+        dtype = np.dtype(dtype)
+        if dtype.kind not in 'iu':
+            raise TypeError("'dtype' must be an integer type")
+
         i = np.iinfo(dtype)
         abs_max = 2 ** (i.bits - 1)
         offset = i.min + abs_max
         return (sig * abs_max + offset).clip(i.min, i.max).astype(dtype)
+
+    # конвертируем список в массив numpy
+    def convert2numpy(self, data):
+        np_array = np.array(data)
+        return np_array.astype(np.float32)
+
+    # переопределяем метод join чтобы он возвращал длинну записи
+    def join(self, *args):
+        threading.Thread.join(self, *args)
+        return self.dur
 
 
 if __name__ == "__main__":
@@ -67,23 +89,16 @@ if __name__ == "__main__":
     default_mic = sc.default_microphone()  # Получаем текущий микрофон по умолчанию в системе
     stop_event = threading.Event()  # Event для того чтобы остановить поток который записывает аудио
 
-
     print("file: {0}".format(filename))
     input("Press Enter to start recording")
-    stime = time.time()  # получаем время когда начали запись
     try:
         t = RecordThread(default_mic, default_speaker, filename, stop_event)
-        #t.daemon = True
         t.start()  # запускаем поток для начала записи
         input("Press Enter to stop recording")
         stop_event.set()  # отдаем команду остановки записи
-        print("Stopping record...", end="")
-        duration = time.time() - stime
-        if (duration < 5):
-            t.join()
-            duration = 5
+        print("Stopping record...")
+        duration = t.join()  # получаем длительность записи
         print("Recorded time {0} sec".format(duration))
         time.sleep(1)
     except:
         print("\nStopped by timeout. Recorded time 60 sec")
-
